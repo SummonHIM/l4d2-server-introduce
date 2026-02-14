75b699d73fe811307c0d935465a7b846cfb2284f
SummonHIM <summonhim@qq.com>
2026-02-14 13:25:36 +0800

    fix: 修复名字过长以及标点符号问题

--- forge.config.ts
+++ forge.config.ts
@@ -1,9 +1,64 @@
+import { utils } from '@electron-forge/core'
 import { FusesPlugin } from '@electron-forge/plugin-fuses'
 import { FuseV1Options, FuseVersion } from '@electron/fuses'
 
 export const packagerConfig = {
   asar: true,
   icon: 'src/assets/logo/favicon',
+  appBundleId: utils.fromBuildIdentifier({
+    beta: 'top.summonhim.serenity.l4d2.beta',
+    prod: 'top.summonhim.serenity.l4d2',
+  }),
+  appCopyright:
+    '© 2026 SummonHIM. Left 4 Dead 2 and related assets are trademarks and/or copyrights of Valve Corporation. This project is not affiliated with or endorsed by Valve.',
+  ignore: [
+    // 文件夹
+    '.github/',
+    '.vscode/',
+    'e2e/',
+    'playwright-report/',
+    'public/',
+    'test-results/',
+
+    // tsconfig 文件
+    'tsconfig.json',
+    'env.d.ts',
+    /tsconfig\..*\.json/,
+
+    // vite 相关配置
+    /vite\.config\..*/,
+    /jsconfig.*/,
+    /vitest\.config.*/,
+    /cypress\.config.*/,
+    /playwright\.config.*/,
+    'components.d.ts',
+
+    // package / yarn / lint / prettier 配置
+    'package-lock.json',
+    /pnpm.*/,
+    /\.yarnrc.*/,
+    /yarn.*/,
+    /\.eslint.*/,
+    /eslint.*/,
+    /\.oxlint.*/,
+    /oxlint.*/,
+    /\.oxfmt.*/,
+    /\.prettier.*/,
+    /prettier.*/,
+    /\.editorconfig/,
+
+    // 环境变量 .env
+    '.env',
+    /\.env\..*/,
+
+    // Git
+    '.gitattributes',
+    '.gitignore',
+    '.npmrc',
+
+    // Electron
+    'forge.config.ts',
+  ],
 }
 export const rebuildConfig = {}
 export const makers = [
