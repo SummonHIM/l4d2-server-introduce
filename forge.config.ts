ca21ab2a6e806503b3712d84852c3a581e687dc0
SummonHIM <summonhim@qq.com>
2026-02-14 17:46:37 +0800

    fix: 修复deb无法找到执行文件的问题

--- forge.config.ts
+++ forge.config.ts
@@ -6,6 +6,8 @@ import 'dotenv/config'
 const currentYear = new Date().getFullYear()
 const serverAddr = process.env.VITE_SRCDS_SERVER_ADDRESS ?? 'example.com'
 const serverProvider = process.env.VITE_SRCDS_SERVER_PROVIDER ?? 'Somebody'
+const executableName =
+  serverProvider == 'SummonHIM' ? 'summonl4d2-server-launcher' : 'l4d2-server-introduce'
 
 function domainToBundleId(domain: string): string {
   return domain.split('.').reverse().join('.')
@@ -13,8 +15,7 @@ function domainToBundleId(domain: string): string {
 
 export const packagerConfig = {
   asar: true,
-  executableName:
-    serverProvider == 'SummonHIM' ? 'summonl4d2-server-launcher' : 'l4d2-server-introduce',
+  executableName: executableName,
   icon: 'src/assets/logo/favicon',
   appBundleId: utils.fromBuildIdentifier({
     beta: domainToBundleId(serverAddr) + '.beta',
@@ -80,6 +81,7 @@ export const makers = [
     name: '@electron-forge/maker-deb',
     config: {
       options: {
+        bin: executableName,
         categories: ['Game'],
         icon: 'src/assets/logo/favicon.png',
       },
